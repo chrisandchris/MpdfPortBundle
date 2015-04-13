8fa2734b0d406bf8c2025600dbfd1445c743787a
Christian Klauenbösch <christian@klit.ch>
2015-04-13 09:42:43 +0200

    replaced mpdf service class to enhanced response handling

--- Service/MpdfService.php
+++ Service/MpdfService.php
@@ -4,85 +4,46 @@ namespace TFox\MpdfPortBundle\Service;
 use Symfony\Component\HttpFoundation\Response;
 
 class MpdfService {
-	private $addDefaultConstructorArgs = true;
-	
-	/**
-	 * Get an instance of mPDF class
-	 * @param array $constructorArgs arguments for mPDF constror
-	 * @return \mPDF
-	 */
-	public function getMpdf($constructorArgs = array()) 
-	{
-		$allConstructorArgs = $constructorArgs;
-		if($this->getAddDefaultConstructorArgs()) {
-			$allConstructorArgs = array_merge(array('utf-8', 'A4'), $allConstructorArgs);
-		}		
 
-		$reflection = new \ReflectionClass('\mPDF');
-		$mpdf = $reflection->newInstanceArgs($allConstructorArgs);
-		
-		return $mpdf;
-	}
-	
-	/**
-	 * Returns a string which content is a PDF document
-	 */
-	public function generatePdf($html, array $argOptions = array())
-	{
-		//Calculate arguments
-		$defaultOptions = array(
-			'constructorArgs' => array(),
-			'writeHtmlMode' => null,
-			'writeHtmlInitialise' => null,
-			'writeHtmlClose' => null,
-			'outputFilename' => '',
-  			'outputDest' => 'S',
-			'mpdf' => null
-		);                
-		$options = array_merge($defaultOptions, $argOptions);
-		extract($options);
+    /**
+     * Get an instance of mPDF class
+     * @param array $constructorArgs arguments for mPDF constructor
+     * @return \mPDF
+     */
+    public function getMpdf($constructorArgs = array('utf-8')) {
+        $reflection = new \ReflectionClass('\mPDF');
+        /** @var \mPDF $mpdf */
+        $mpdf = $reflection->newInstanceArgs($constructorArgs);
+        $mpdf->allow_charset_conversion = false;
+        $mpdf->debug = true;
+        return $mpdf;
+    }
 
-		if(null==$mpdf)
-			$mpdf = $this->getMpdf($constructorArgs);
-		
-		//Add argguments to AddHtml function
-		$writeHtmlArgs = array($writeHtmlMode, $writeHtmlInitialise, $writeHtmlClose);
-		$writeHtmlArgs = array_filter($writeHtmlArgs, function($x) { return !is_null($x); });
-		$writeHtmlArgs['html'] = $html;
-		
-		@call_user_func_array(array($mpdf, 'WriteHTML'), $writeHtmlArgs);
-		
-		//Add arguments to Output function
-        $content = $mpdf->Output($outputFilename, $outputDest);
-		return $content;
-	}
-	
-	/**
-	 * Generates an instance of Response class with PDF document
-	 * @param unknown $argContent
-	 * @param array $argOptions
-	 */
-	public function generatePdfResponse($html, array $argOptions = array())
-	{
-		$response = new Response();		
-		$response->headers->set('Content-Type', 'application/pdf');
-		
-		$content = $this->generatePdf($html, $argOptions);
-		$response->setContent($content);
-		
-		return $response;
-	}
-	
-	//Getters and setters
-	
-	public function setAddDefaultConstructorArgs($val)
-	{
-		$this->addDefaultConstructorArgs = $val;
-		return $this;
-	}
-	
-	public function getAddDefaultConstructorArgs()
-	{
-		return $this->addDefaultConstructorArgs;
-	}
+    /**
+     * @param \mPDF $mPDF
+     * @param $html
+     * @param string $filename
+     * @param bool $debug
+     * @return Response
+     */
+    public function getResponse(\mPDF $mPDF, $html, $filename = 'file', $debug = false) {
+        $response = new Response();
+        $response->setStatusCode(200);
+        $response->headers->set('Pragma', 'no-cache', true);
+        if ($debug OR $_GET['debug'] == true) {
+            $debug = true;
+            $response->headers->set('Content-Type', 'text/html', true);
+        } else {
+            $response->headers->set('Content-Type', 'application/pdf', true);
+        }
+        $response->headers->set('Content-Disposition', 'inline; filename=' . $filename . '.pdf', true);
+
+        if ($debug) {
+            $response->setContent($html);
+        } else {
+            $mPDF->WriteHTML($html);
+            $response->setContent($mPDF->Output(null, 'S'));
+        }
+        return $response;
+    }
 }
